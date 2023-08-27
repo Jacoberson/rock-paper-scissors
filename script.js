8f92b72a0b37c2f4c027c1225877d68ef1457975
Jacoberson <c.jacob.moore92@gmail.com>
2023-08-27 12:47:11 -0500

    Add more DOM elements
    - Added elements to display players' choices
    - Removed function to get player's choice
    - Extracted some code to its own functions

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ const getComputerChoice = () => {
     return options[Math.floor(Math.random() * options.length)];
 }
 
-const getPlayerChoice = () => {
-    let playerChoice = prompt('Choose rock, paper, or scissors');
-    return playerChoice.toLowerCase();
-}
-
 const playRound = (computerChoice, playerChoice) => {
     if ((computerChoice === 'rock' && playerChoice === 'scissors') ||
         (computerChoice === 'scissors' && playerChoice === 'paper') ||
@@ -22,6 +17,16 @@ const playRound = (computerChoice, playerChoice) => {
         (computerChoice === 'scissors' && playerChoice === 'rock')) {
         playerScore++;
     } 
+    displayChoices(computerChoice, playerChoice);
+    updateScore();
+}
+
+const displayChoices = (computerChoice, playerChoice) => {
+    document.getElementById('computer_choice').textContent = `Computer Choice: ${computerChoice}`;
+    document.getElementById('player_choice').textContent = `Player Choice: ${playerChoice}`;
+}
+
+const updateScore = () => {
     document.getElementById('computer_score').textContent = `Computer Score: ${computerScore}`
     document.getElementById('player_score').textContent = `Player Score: ${playerScore}`
 }
@@ -38,12 +43,20 @@ const div = document.createElement('div');
 container.appendChild(div);
 
 const computerScoreOutput = document.createElement('p');
-computerScoreOutput.setAttribute('id', 'computer_score')
-computerScoreOutput.textContent = `Computer Score: ${computerScore}`
+computerScoreOutput.setAttribute('id', 'computer_score');
+computerScoreOutput.textContent = `Computer Score: ${computerScore}`;
+
+const computerChoiceOutput = document.createElement('p');
+computerChoiceOutput.setAttribute('id', 'computer_choice');
 
 const playerScoreOutput = document.createElement('p');
 playerScoreOutput.setAttribute('id', 'player_score');
-playerScoreOutput.textContent = `Player Score: ${playerScore}`
+playerScoreOutput.textContent = `Player Score: ${playerScore}`;
+
+const playerChoiceOutput = document.createElement('p');
+playerChoiceOutput.setAttribute('id', 'player_choice');
 
 div.appendChild(computerScoreOutput);
+div.appendChild(computerChoiceOutput);
 div.appendChild(playerScoreOutput);
+div.appendChild(playerChoiceOutput);
